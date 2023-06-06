67b8d38950a612d2c1221f177a9d381871712e10
Mikael Pettersson <evanion@icloud.com>
2023-06-06 23:46:47 +0200

    Initial Commit

---
+++ Taskmaster/TaskmasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskmasterApp.swift
+//  Taskmaster
+//
+//  Created by Mikael Pettersson on 2023-06-06.
+//
+
+import SwiftUI
+
+@main
+struct TaskmasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
